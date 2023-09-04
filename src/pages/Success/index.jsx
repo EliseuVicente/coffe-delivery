0fbaab828bc2bf702fb83d262cd47c0de2395d9f
Eliseu Vicente <eliseu965@gmail.com>
2023-09-04 16:36:19 -0300

    Create page Success

---
+++ src/pages/Success/index.jsx
@@ -0,0 +1,51 @@
+import { useContext } from "react"
+import { useLocation } from "react-router-dom"
+import { ProductsContext } from "../../contexts/ProductsContext"
+import { Container, ContainerInfo, H1, H2, Image, LabelInformation } from "./styles"
+import { CicleYellowDark, CirclePurple, CircleYellow } from "../Home/styles"
+import { MapPin, Timer, CurrencyDollarSimple } from "phosphor-react"
+
+export function Success(){
+
+const {selectOption} = useContext(ProductsContext)
+
+const location = useLocation()
+const formData = location.state
+
+return(
+    <>
+        <H1>Uhu! Pedido confirmado</H1>
+        <H2>Agora é só aguardar que logo o café chegará até você</H2>
+        <Container>
+        <ContainerInfo>
+
+            <LabelInformation>
+                <CirclePurple>
+                    <MapPin size={16} color="#FAFAFA" weight="fill" />
+                </CirclePurple>
+                <p>Entrega em <strong>{formData.street}, {formData.number}</strong> {formData.neighborhood} - {formData.city}, {formData.uf}</p>
+            </LabelInformation>
+
+            <LabelInformation>
+                <CircleYellow>
+                    <Timer size={16} color="white" weight="fill"/>
+                </CircleYellow>
+                <p>Previsão de entrega <br/><strong>20 min - 30 min</strong></p>     
+            </LabelInformation>
+
+            <LabelInformation>
+                <CicleYellowDark>
+                    <CurrencyDollarSimple size={16} color="white" weight="fill" />
+                </CicleYellowDark>
+                <p>Pagamento na entrega <br/> <strong>{selectOption}</strong></p>
+            </LabelInformation>
+
+        </ContainerInfo>
+        
+        <Image src="../../src/assets/illustration.svg"/>
+        </Container>
+    </>
+    )
+        
+    
+}
---
+++ src/pages/Success/styles.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+    margin-top: 50px;
+    padding-left: 10rem;
+    color: var(--brand-yellow-dark, #C47F17);
+    font-family: Baloo 2;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: 130%;
+`
+
+export const H2 = styled.h2`
+    padding-left: 10rem;
+    color: var(--base-subtitle, #403937);
+    font-family: Roboto;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%; /* 26px */
+`
+export const Container = styled.div`
+    display: flex;
+    margin-top: 40px;
+    padding-left: 10rem;
+    gap: 102px
+`
+export const ContainerInfo = styled.div`
+    display: flex;
+    margin-top: 40px;
+    width: 526px;
+    padding: 40px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 32px;
+
+    border-radius: 6px 36px;
+    border: 1px solid #DBAC2C;
+`
+
+export const Image = styled.img`
+    width: 492px;
+    height: 293px;
+    flex-shrink: 0;
+`
+export const LabelInformation = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;  
+
+    p{
+        width: 20rem;
+        font-family: Roboto;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        
+        strong{
+        color: var(--base-text, #574F4D);
+        font-family: Roboto;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 700;
+        }
+    }
+
+    
+`
