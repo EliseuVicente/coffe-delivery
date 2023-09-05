b0e5ed4b70cddd634820edf301d6c4ec5d2d554c
Eliseu Vicente <eliseu965@gmail.com>
2023-09-05 13:44:05 -0300

    insert logo like a Component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom"
 import { useContext } from "react"
 import { ProductsContext } from "../../contexts/ProductsContext"
 
+import  Logo  from "../../assets/logo.svg"
+
 export function NavbarHeader(){
 
     const { cartItem } = useContext(ProductsContext)
    return (
     <Navbar>
         <Link to={'/'}>
-            <img src='\src\assets\logo.svg'/>
+            <img src={Logo}/>
         </Link>
         
         <Action>
